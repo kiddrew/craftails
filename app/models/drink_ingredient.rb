078170d1bff3bbf80f8f0b78b0eace3e3348302e
Andrew Hunter <hunteraa@gmail.com>
2014-08-31 17:18:43 -0500

    rinse

--- app/models/drink_ingredient.rb
+++ app/models/drink_ingredient.rb
@@ -2,7 +2,7 @@ class DrinkIngredient < ActiveRecord::Base
   belongs_to :drink
   belongs_to :ingredient
 
-  UNITS = ['oz','tsp','tbsp','mL','dash', 'barspoon']
+  UNITS = ['oz','tsp','tbsp','mL','dash', 'barspoon','rinse']
 
   validates_presence_of :amount
   validates_presence_of :amount_unit
