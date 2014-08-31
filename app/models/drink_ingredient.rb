e34a325dc7941d77b7e56161bc241864af386a8b
Andrew Hunter <hunteraa@gmail.com>
2014-08-31 17:14:15 -0500

    barspoon

--- app/models/drink_ingredient.rb
+++ app/models/drink_ingredient.rb
@@ -2,7 +2,7 @@ class DrinkIngredient < ActiveRecord::Base
   belongs_to :drink
   belongs_to :ingredient
 
-  UNITS = ['oz','tsp','tbsp','mL','dash']
+  UNITS = ['oz','tsp','tbsp','mL','dash', 'barspoon']
 
   validates_presence_of :amount
   validates_presence_of :amount_unit
