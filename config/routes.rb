4d3d98d278f6f6df8b3e6f07962ee254540fd9e4
Andrew Hunter <hunteraa@gmail.com>
2014-09-01 15:51:48 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
 
-  resources :drinks, only: [:new, :create, :show, :index]
-  resources :ingredients, only: [:new, :create, :show, :index]
+  resources :drinks
+  resources :ingredients
 
   resources :bars, only: [:update]
   get 'my_bar' => 'bars#edit'
