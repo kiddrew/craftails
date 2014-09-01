e01e3954da20d1907102f746f2e08c5191727c1e
Andrew Hunter <hunteraa@gmail.com>
2014-09-01 16:04:34 -0500

    drink edit works

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,15 +1,17 @@
 class DrinksController < InheritedResources::Base
+  respond_to :html, :json, :except => [:destroy]
   before_filter :authenticate_user!, only: [:new, :create]
+  before_filter :authenticate_admin!, only: [:edit, :update]
+  before_action :prepare_params, only: [:create, :update]
+
+  def edit
+    @drink = Drink.find(params['id'])
+    (6-@drink.ingredients.count).times do
+      @drink.drink_ingredients.build
+    end
+  end
 
   def create
-    tmp = {}
-    params['drink']['drink_ingredients_attributes'].each do |k,v|
-      if v['amount'].present? and v['amount_unit'].present? and v['ingredient_id'].present?
-        tmp[k] = v
-      end
-    end
-    params['drink']['drink_ingredients_attributes'] = tmp
-
     @drink = build_resource
     @drink.user_id = current_user
 
@@ -26,8 +28,18 @@ class DrinksController < InheritedResources::Base
 
   def random
     @drink = Drink.order("random()").first
+  end
 
-    
+  private
+
+  def prepare_params
+    tmp = {}
+    params['drink']['drink_ingredients_attributes'].each do |k,v|
+      if v['amount'].present? and v['amount_unit'].present? and v['ingredient_id'].present?
+        tmp[k] = v
+      end
+    end
+    params['drink']['drink_ingredients_attributes'] = tmp
   end
 
   def permitted_params
