fd75f5e9d91b68a8031bb37ae3a24790a5dc1f68
Andrew Hunter <hunteraa@gmail.com>
2014-09-02 22:02:48 -0500

    update

--- app/helpers/drinks_helper.rb
+++ app/helpers/drinks_helper.rb
@@ -1,6 +1,6 @@
 module DrinksHelper
   def drink_image image_url
-    if image_url
+    if image_url.present?
       return image_tag image_url, :class => "img-polaroid"
     else
       return image_tag 'http://www.placehold.it/200x150/EFEFEF/AAAAAA&text=no+image', :class => "img-polaroid"
