50626fb2144a930bf9e4bc7fe503d7db8ac4381b
Andrew Hunter <hunteraa@gmail.com>
2014-09-06 16:50:23 -0500

    amount unit no longer required

--- app/models/drink_ingredient.rb
+++ app/models/drink_ingredient.rb
@@ -2,9 +2,8 @@ class DrinkIngredient < ActiveRecord::Base
   belongs_to :drink
   belongs_to :ingredient
 
-  UNITS = ['oz','tsp','tbsp','mL','dash', 'barspoon','rinse']
+  UNITS = ['oz','tsp','tbsp','dash', 'barspoon','rinse']
 
   validates :ingredient_id, uniqueness: {scope: :drink_id}, presence: true
   validates_presence_of :amount
-  validates_presence_of :amount_unit
 end
